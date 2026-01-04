aa3d74cafad7e2e21ec47ad75e42071de9732f33
ZimoSyndrome <97791066+ZimoSyndrome@users.noreply.github.com>
2026-01-03 19:48:28 -0500

    initial skeleton

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import HeroSection from './components/HeroSection';
+import ProjectsSection from './components/ProjectsSection';
+import SkillsSection from './components/SkillsSection';
+import ContactSection from './components/ContactSection';
+import ThemeToggle from './components/theme/ThemeToggle';
+
+export default function MinimalModernPortfolio() {
+	return (
+		<div className="relative min-h-screen bg-white text-black dark:bg-black dark:text-white">
+			<div className="absolute top-4 right-4 z-10">
+				<ThemeToggle />
+			</div>
+			<HeroSection />
+			<ProjectsSection />
+			<SkillsSection />
+			<ContactSection />
+		</div>
+	);
+}
